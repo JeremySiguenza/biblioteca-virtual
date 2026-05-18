d6aa148de6e81baae7c0015dbc9896a82af0165c
Jeremy5405 <jsiguenza273@gmail.com>
2026-05-17 21:38:59 -0600

    allowed hosts

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*-9*b1072=eht4*3$-we%25xv!x8l2nv2vqc%66x)w)&8)ev6+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
